072a792a822c9ba89e31d20ffd9711ae5358d0cf
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 15:34:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,8 @@ class Anagram
   
   def match(find_anagram_in_this_string) 
     puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word| 
-      if word.split("").sort == self.word_to_analyze.split("").sort 
+    find_anagram_in_this_string.find_all do |given_word| 
+      if given_wordword.split("").sort == self.word_to_analyze.split("").sort 
         word 
       end
     end 
